33b4712115809ee98e062105139a2c3f34dfa63c
geek <1163518793@qq.com>
2020-01-03 09:35:48 +0800

    Initial commit

---
+++ src/test/java/com/example/videoparse/VideoParseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videoparse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoParseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
